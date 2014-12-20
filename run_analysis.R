f81d5041f04025036fd49f01058dae98c0822884
Youssef Shahin <yshahin@gmail.com>
2014-12-20 13:16:54 +0200

    Updates script and ignore produced output file

--- run_analysis.R
+++ run_analysis.R
@@ -1,65 +1,71 @@
 library(tidyr)
 library(dplyr)
+setwd("~/Developer/work/DataScience/getdata-016-project")
 
-
-header.phone.data <- function(directory, type) {
+header.phone.data <- function(directory) {
   ## read feature headers from features file
   ## and select only the feature names
   feature_header <- read.table(
-    paste0(directory, "/features.txt"), 
+    paste0(directory, "/features.txt"),
     header = F, sep = " ", quote = "", comment.char = "")[,2]
   ## Append feature count due to duplicated feature names
   feature_header <- mapply(paste0, seq_along(feature_header), ".",
-    sapply(feature_header, function(x) gsub("\\.+", ".", make.names(tolower(x)))))
+    sapply(feature_header, function(x) {
+      sub("bodybody", "body", gsub("\\.+", ".", make.names(tolower(x))))
+    }))
   feature_header
 }
 
 ## load data assuming they are in a subfolder
 ## in your working directory
-phone.data <- function(directory, type) {
-  feature_header <- header.phone.data(directory, type)
+phone.data <- function(directory, type=F) {
+  feature_header <- header.phone.data(directory)
   ## read data
   data <- read.fwf(
     paste0(directory, "/", type,
-      paste0("/X_", type, ".txt.sample")), 
-    rep(16, length(feature_header)), 
+      paste0("/X_", type, ".txt")),
+    rep(16, length(feature_header)), colClasses=rep("numeric", length(feature_header)),
     header = F, comment.char = "")
   names(data) <- feature_header
   data
 }
 
-subject.phone.data <- function(directory, type) {
+subject.phone.data <- function(directory, type=F) {
   ## read subject id
   data <- read.table(
     paste0(directory, "/", type,
-           paste0("/subject_", type, ".txt.sample")), 
+           paste0("/subject_", type, ".txt")),
     header = F, sep = " ", quote = "", comment.char = "")
   names(data) <- "subject"
   data
 }
 
-activity.phone.data <- function(directory, type) {
+activity.phone.data <- function(directory, type=F) {
   ## read activity ids
   activityids <- read.table(
-    paste0(directory, "/", type, 
-           paste0("/y_", type, ".txt.sample")),
+    paste0(directory, "/", type,
+           paste0("/y_", type, ".txt")),
     header = F, sep = " ", quote = "", comment.char = "")
 
   ##load activity names
   activitynames <- read.table(
-    paste0(directory, "/activity_labels.txt"), 
+    paste0(directory, "/activity_labels.txt"),
     header = F, sep = " ", quote = "", stringsAsFactors = F, comment.char = "")[,2]
-  
+
   data <- sapply(activityids, function(x) activitynames[x])
   colnames(data) <- "activity"
   data
 }
 
-prepare.phone.data <- function(directory) {
+prepare.phone.data <- function(directory=F) {
   # x data with appropriate labels
+  ## select only the needed columns which are the columns that
+  ## contain mean, std and the activity names
   data <- rbind( # Merge training and test data
-      phone.data("sample", "train"), 
-      phone.data("sample", "test"))
+    select(phone.data(directory, "train"), 
+           contains(".mean."), contains(".std.")),
+    select(phone.data(directory, "test"), 
+           contains(".mean."), contains(".std.")))
   # subject column with appropriate label
   data <- cbind(
     rbind(subject.phone.data(directory, "train"),
@@ -71,13 +77,11 @@ prepare.phone.data <- function(directory) {
   data
 }
 ## convert to table data.frame for use with dplyr and tidyr
-data <- tbl_df(prepare.phone.data("sample"))
+data <- tbl_df(prepare.phone.data("UCI HAR Dataset"))
 
-## select only the needed columns which are the columns that
-## contain mean, std and the activity names
-data <- select(data, subject, contains(".mean."), contains(".std."), activity)
+data <- data %>%
+  gather(key, value, -activity, -subject) %>%
+  separate(key, c("featureid", "measurement", "aggregation", "axis")) %>%
+  select(-featureid) %>% separate(measurement, c("domain", "measurment"), sep = 1)
 
-#write.table(data, "data_table.txt", row.names=F)
-temp <- data %>% 
-  gather(key, value, -activity, -subject)
-
+write.table(data, "data_table.txt", row.names=F)
